cddb76f9ab19ce0ea1c566e23d6aa2854146670a
Defetysta <tadeusznowak95@gmail.com>
2020-06-11 12:12:23 +0200

    Created main menu and game scenes; added difficulty level selection screen

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public static SceneLoader Instance;
+    public enum DifficultyLevels { Easy, Medium, Hard };
+    internal DifficultyLevels difficultyLevel = DifficultyLevels.Easy;
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+    }
+    public void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex >= SceneManager.sceneCountInBuildSettings - 2)
+            currentSceneIndex = 0;
+
+
+        SceneManager.LoadScene(currentSceneIndex + 1);
+
+    }
+
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+    public void ChooseDifficulty(DifficultyLevels desiredLevel)
+    {
+        difficultyLevel = desiredLevel;
+    }    
+    public void ChooseDifficulty(int desiredLevel)
+    {
+        difficultyLevel = (DifficultyLevels)desiredLevel;
+        Debug.Log(difficultyLevel);
+    }
+}
